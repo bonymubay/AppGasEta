155b2c5af5553ac7295de1a61b682f4a37b6c4db
bony mubay <bonymubay@gmail.com>
2023-06-23 14:15:49 +0200

    removeMethodOnUtillClass

--- app/src/main/java/curso/bony/appgaseta/apoio/UtilGasEta.java
+++ app/src/main/java/curso/bony/appgaseta/apoio/UtilGasEta.java
@@ -5,12 +5,5 @@ import java.text.DecimalFormat;
 public class UtilGasEta {
     static DecimalFormat df=new DecimalFormat("#.##");
 
-    public static String calcularGasto(double distancia, double kilometrosPorLitro, double precoPorLitro){
-
-
-        double numeroDeLitros=  distancia / kilometrosPorLitro; //calcular numero de litros pra uma dada distancia
-
-            double gasto= numeroDeLitros*precoPorLitro;
-        return df.format(gasto);
-    }
+ 
 }
